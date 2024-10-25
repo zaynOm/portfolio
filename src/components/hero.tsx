35f3ee6ebd5159635278f34e70035600f98a60f2
zaynom <ouaziz0626@gmail.com>
2024-10-25 22:14:05 +0100

    Add Hero Component

---
+++ src/components/hero.tsx
@@ -0,0 +1,16 @@
+export default function Hero() {
+  return (
+    <div>
+      <h1 className="text-8xl font-bold">
+        Hi! I&apos;m Omar
+        <br />
+        Ouaziz
+      </h1>
+      <p className="text-xl font-bold font-mono text-yellow-200 tracking-wider max-w-screen-md text-balance">
+        Full Stack Developer with a passion for building dynamic web and mobile
+        experiences. From seamless front-end designs to efficient back-end
+        solutions, I craft user-focused applications with precision.
+      </p>
+    </div>
+  );
+}
